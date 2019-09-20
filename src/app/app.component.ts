c1b4dcb2c33cc8382e711a1024c9f86038e8a1da
viveksacademia4git <viveksacademia@gmail.com>
2019-09-20 19:39:15 +0200

    Implementation of Neovis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NeoVis } from 'neovis.js'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'neovis-test';
+
+  title = 'Neovis-Anuglar-Test';
+
+  config = {
+    container_id: "viz",
+    server_url: "bolt://localhost:7687",
+    server_user: "neo4j",
+    server_password: "sorts-swims-burglaries",
+    labels: {
+        //"Character": "name",
+        "Character": {
+            "caption": "name",
+            "size": "pagerank",
+            "community": "community"
+            //"sizeCypher": "MATCH (n) WHERE id(n) = {id} MATCH (n)-[r]-() RETURN sum(r.weight) AS c"
+        }
+    },
+    relationships: {
+        "INTERACTS": {
+            "thickness": "weight",
+            "caption": false
+        }
+    },
+    initial_cypher: "MATCH (n)-[r:INTERACTS]->(m) RETURN n,r,m"
+  };
+
+  viz = new NeoVis.default(this.config);
+  // viz.render();
 }
+
+
