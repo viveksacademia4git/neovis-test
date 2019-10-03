7c1fb9e836121df33b5128a239384d08e9dea8a9
thebestnom <shoval.arad@gmail.com>
2019-10-03 21:44:31 +0300

    small fix, will work after new version out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,43 @@
-import { Component } from '@angular/core';
-import { NeoVis } from 'neovis.js'
+import {Component, OnInit} from '@angular/core';
+import NeoVis from 'neovis.js';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  title = 'Neovis-Anuglar-Test';
+    title = 'Neovis-Anuglar-Test';
 
-  config = {
-    container_id: "viz",
-    server_url: "bolt://localhost:7687",
-    server_user: "neo4j",
-    server_password: "sorts-swims-burglaries",
-    labels: {
-        //"Character": "name",
-        "Character": {
-            "caption": "name",
-            "size": "pagerank",
-            "community": "community"
-            //"sizeCypher": "MATCH (n) WHERE id(n) = {id} MATCH (n)-[r]-() RETURN sum(r.weight) AS c"
-        }
-    },
-    relationships: {
-        "INTERACTS": {
-            "thickness": "weight",
-            "caption": false
-        }
-    },
-    initial_cypher: "MATCH (n)-[r:INTERACTS]->(m) RETURN n,r,m"
-  };
+    config = {
+        container_id: 'viz',
+        server_url: 'bolt://localhost:7687',
+        server_user: 'neo4j',
+        server_password: 'sorts-swims-burglaries',
+        labels: {
+            // "Character": "name",
+            Character: {
+                caption: 'name',
+                size: 'pagerank',
+                community: 'community'
+                // "sizeCypher": "MATCH (n) WHERE id(n) = {id} MATCH (n)-[r]-() RETURN sum(r.weight) AS c"
+            }
+        },
+        relationships: {
+            INTERACTS: {
+                thickness: 'weight',
+                caption: false
+            }
+        },
+        initial_cypher: 'MATCH (n)-[r:INTERACTS]->(m) RETURN n,r,m'
+    };
 
-  viz = new NeoVis.default(this.config);
-  // viz.render();
+    viz = new NeoVis(this.config);
+
+    ngOnInit(): void {
+        this.viz.render();
+    }
 }
 
 
